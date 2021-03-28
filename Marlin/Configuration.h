839b1b6282597040e09326bc6ed53a1af6224da4
Nathan Peckham <59847922+nmpeckham@users.noreply.github.com>
2021-03-28 16:28:29 -0230

    Update bed size for y-axis linear rail conversion

--- Marlin/Configuration.h
+++ Marlin/Configuration.h
@@ -1235,7 +1235,7 @@
 
 // The size of the print bed
 #define X_BED_SIZE 235
-#define Y_BED_SIZE 235
+#define Y_BED_SIZE 220
 
 // Travel limits (mm) after homing, corresponding to endstop positions.
 #define X_MIN_POS 0
@@ -1243,7 +1243,7 @@
 #define Z_MIN_POS 0
 #define X_MAX_POS X_BED_SIZE
 #define Y_MAX_POS Y_BED_SIZE
-#define Z_MAX_POS 250
+#define Z_MAX_POS 240
 
 /**
  * Software Endstops
